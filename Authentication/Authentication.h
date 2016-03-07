14a58027ea89d30c446ee0605647bc1596d305c0
Guido Marucci Blas <guidomb@gmail.com>
2016-03-07 11:32:16 -0300

    Initial commit.

---
+++ Authentication/Authentication.h
@@ -0,0 +1,19 @@
+//
+//  Authentication.h
+//  Authentication
+//
+//  Created by Guido Marucci Blas on 3/7/16.
+//  Copyright © 2016 Wolox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Authentication.
+FOUNDATION_EXPORT double AuthenticationVersionNumber;
+
+//! Project version string for Authentication.
+FOUNDATION_EXPORT const unsigned char AuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Authentication/PublicHeader.h>
+
+
