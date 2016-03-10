7d411bd2830fc02ebe00757bdc133cdd6796ae05
danielaRiesgo <danielap.riesgo@gmail.com>
2016-03-10 11:45:14 -0300

    Encryptedtravis variables

--- AuthenticationTests/LogIn/LogInViewModelTests.swift
+++ AuthenticationTests/LogIn/LogInViewModelTests.swift
@@ -1,16 +1,15 @@
 //
-//  AuthenticationTests.swift
-//  AuthenticationTests
+//  LogInViewModelTests.swift
+//  Authentication
 //
-//  Created by Guido Marucci Blas on 3/7/16.
+//  Created by Daniela Riesgo on 3/10/16.
 //  Copyright © 2016 Wolox. All rights reserved.
 //
 
 import XCTest
-@testable import Authentication
 
-class AuthenticationTests: XCTestCase {
-    
+class LogInViewModelTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +19,17 @@ class AuthenticationTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
